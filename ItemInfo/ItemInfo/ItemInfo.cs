a07612beb91d0ba09d25a4f66c5728bf37a9fc1f
BluePickaxe <sungwanim@naver.com>
2017-03-26 23:32:21 +0900

    Item 정보 관련해서 Id, Name, Count 모두 잘 표시하게 변경

--- ItemInfo/ItemInfo/ItemInfo.cs
+++ ItemInfo/ItemInfo/ItemInfo.cs
@@ -22,17 +22,16 @@ namespace ItemInfo
             Console.WriteLine("[ItemInfo] >> Enable!");
         }
         [Command(Name = "iv", Description = "Show Item Info")]
-        public void iv(Player player)
+        public void Execute(Player player)
         {
-            var id = player.Inventory.GetItemInHand().Id;
-            /* var minetname = player.Inventory.GetItemInHand().ToString();
-            string Sep = ".";
-            char[] Seps = Sep.ToArray();
-            string[] strs = minetname.Split(Seps, StringSplitOptions.RemoveEmptyEntries);
-            string name = strs[2];  */
+            var handit = player.Inventory.GetItemInHand();
+            var id = handit.Id;
+            var name = handit.GetType().Name;
+            var count = handit.Count;
 
             player.SendMessage($"§9ItemInfo > §fItem Id: §7{id}");
-            //player.SendMessage($"§9ItemInfo > §fItem Name: §7{player.Inventory.GetItemInHand().ToString()}");
+            player.SendMessage($"§9ItemInfo > §fItem Name: §7{name}");
+            player.SendMessage($"§9ItemInfo > §fItem Count: §7{count}");
         }
     }
 }
