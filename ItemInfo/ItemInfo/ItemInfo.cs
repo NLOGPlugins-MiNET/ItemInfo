9d57cef5e8a94329cb95dc89dc0aff4215d1b356
푸른곡괭이 <sungwanim@naver.com>
2017-03-27 15:56:49 +0900

    Revert "클래스 명 변경 및 상대방이 들고 있는 아이템 정보 볼 수 있게 수정"
    This reverts commit 7549896ec13242c833377381150c2b2555cba2e0.

--- ItemInfo/ItemInfo/ItemInfo.cs
+++ ItemInfo/ItemInfo/ItemInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 using MiNET;
 using MiNET.Plugins;
@@ -7,7 +7,7 @@ using MiNET.Plugins.Attributes;
 namespace ItemInfo
 {
     [Plugin]
-    public class ItemInfo : Plugin
+    public class Class1 : Plugin
     {
         protected override void OnEnable()
         {
@@ -26,24 +26,5 @@ namespace ItemInfo
             player.SendMessage($"§9ItemInfo > §fItem Name: §7{name}");
             player.SendMessage($"§9ItemInfo > §fItem Count: §7{count}");
         }
-	[Command(Name = "iv")]
-	public void Execute(Player player, string args)
-	{
-	    foreach (var i in Context.LevelManager.Levels.ToList())
-	    {
-	        if (args == i.Value.Username)
-		{
-		    var handit = i.Value.Inventory.GetItemInHand();
-            	    var id = handit.Id;
-            	    var name = handit.GetType().Name;
-            	    var count = handit.Count;
-
-            	    i.Value.SendMessage($"§9ItemInfo > §fItem Id: §7{id}");
-            	    i.Value.SendMessage($"§9ItemInfo > §fItem Name: §7{name}");
-            	    i.Value.SendMessage($"§9ItemInfo > §fItem Count: §7{count}");
-		    break;	
-		}
-	    }
-	}
     }
 }
