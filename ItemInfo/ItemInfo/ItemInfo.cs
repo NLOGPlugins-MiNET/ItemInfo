69e9cac936ea89f3517e8312f5bf40c813dc594a
BluePickaxe <sungwanim@naver.com>
2017-03-27 20:25:02 +0900

    아까 잘못 커밋한 부분 수정

--- ItemInfo/ItemInfo/ItemInfo.cs
+++ ItemInfo/ItemInfo/ItemInfo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 using MiNET;
 using MiNET.Plugins;
@@ -17,6 +18,23 @@ namespace ItemInfo
         [Command(Name = "iv", Description = "Show Item Info")]
         public void Execute(Player player)
         {
+            var handit = player.Inventory.GetItemInHand();
+            var id = handit.Id;
+            var name = handit.GetType().Name;
+            var count = handit.Count;
+
+            player.SendMessage($"§9ItemInfo > §fItem Id: §7{id}");
+            player.SendMessage($"§9ItemInfo > §fItem Name: §7{name}");
+            player.SendMessage($"§9ItemInfo > §fItem Count: §7{count}");
+        }
+        [Command(Name = "iv", Description = "Show Item Info")]
+        public void Execute(Player sender, string username)
+        {
+            Player player = null;
+            foreach (var i in Context.LevelManager.Levels)
+            {
+                player = i.Players.ToList().Find(x => x.Value.Username.ToLower().Contains(username)).Value ?? null; break;
+            }
             var handit = player.Inventory.GetItemInHand();
             var id = handit.Id;
             var name = handit.GetType().Name;
